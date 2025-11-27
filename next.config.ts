f6ab7c6a25e9b611b9ea0da689510cb8cf8a70cb
SrilathaChilagani <chilaganisrilatha89@gmail.com>
2025-11-27 13:15:59 -0600

    fix(TypeScript): prevent Vercel from scanning Expo app

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import {withSentryConfig} from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript: {
+    tsconfigPath: "./tsconfig.next.json",
+  },
   async headers() {
     return [
       {
